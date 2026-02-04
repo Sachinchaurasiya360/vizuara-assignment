7950d64cb8f566d6e721f3f3401788cd64017599
Sachin Chaurasiya <mrsachinchaurasiya@gmail.com>
2026-02-04 23:05:02 +0530

    changes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,6 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import { ErrorBoundary } from "./components/ErrorBoundary";
 import { LandingPage } from "./components/LandingPage";
 import { PipelineBuilder } from "./components/PipelineBuilder";
-import { AboutMe } from "./components/AboutMe";
 import { ModelEvaluationPage } from "./modules/model-evaluation/pages/ModelEvaluationPage";
 import { WorkflowBuilderPage } from "./modules/workflow-builder/pages/WorkflowBuilderPage";
 import "./App.css";
@@ -15,7 +14,6 @@ function App() {
         <Route path="/builder" element={<PipelineBuilder />} />
         <Route path="/workflow-builder" element={<WorkflowBuilderPage />} />
         <Route path="/model-evaluation" element={<ModelEvaluationPage />} />
-        <Route path="/about" element={<AboutMe />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </ErrorBoundary>
